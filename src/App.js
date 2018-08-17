de16b1213226331a2bdf48f48e6892239e85933a
joydeeproy87 <mail2joydeep.roy@gmail.com>
2018-08-18 00:00:20 +0530

    React - resume version

--- src/App.js
+++ src/App.js
@@ -1,20 +1,252 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App">
+        <div className="container">
+
+          <div className="clearfix">&nbsp;</div>
+
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <strong>Mobile </strong>
+              <a target="_blank" href="tel:08884713695">08884713695</a>
+              <br />
+              <strong>Email </strong>
+              <a target="_blank" href="mailto:joydeep4jobs@gmail.com">joydeep4jobs@gmail.com</a>
+            </div>
+
+            <div className="col-sm-12 col-md-6">
+              <strong>LinkedIn </strong>
+              <a target="_blank" href="https://www.linkedin.com/in/joydeeproy87/">linkedin.com/in/joydeeproy87/</a>
+              <br />
+              <strong>Github </strong>
+              <a target="_blank" href="https://github.com/joydeeproy87">github.com/joydeeproy87</a>
+            </div>
+          </div>
+
+          <div className="clearfix">&nbsp;</div>
+          <div className="clearfix">&nbsp;</div>
+
+          <div className="row">
+            <div className="col">
+              <h2>JOYDEEP ROY - UI / UX Developer / Web Developer - 6 Years</h2>
+            </div>
+          </div>
+
+          <div className="clearfix">&nbsp;</div>
+          <div className="clearfix">&nbsp;</div>
+
+          <div className="row">
+            <div className="col">
+              <h3>Experience Summary</h3>
+              <ul>
+                <li>Having total of 6+ years experience in IT industry (4.5 years) and Design industry (2 years).</li>
+                <li>
+                  <span className="badge badge-pill badge-primary">HTML5</span>,
+        <span className="badge badge-pill badge-primary">CSS3</span>,
+        <span className="badge badge-pill badge-primary">JavaScript</span>,
+        <span className="badge badge-pill badge-primary">AngularJS(1.x)</span>,
+        <span className="badge badge-pill badge-primary">ES6</span>,
+        <span className="badge badge-pill badge-primary">jQuery</span>,
+        <span className="badge badge-pill badge-primary">Bootstrap</span>,
+        <span className="badge badge-pill badge-primary">Git</span>,
+        <span className="badge badge-pill badge-primary">Sass</span>,
+        <span className="badge badge-pill badge-primary">Milligram</span>,
+        <span className="badge badge-pill badge-primary">WordPress</span>,
+        <span className="badge badge-pill badge-primary">PHP</span> &amp;
+        <span className="badge badge-pill badge-primary">MySQL</span>.
+      </li>
+                <li>
+                  <span className="badge badge-pill badge-primary">Search Engine Optimization</span> (SEO),
+        <span className="badge badge-pill badge-primary">Google Analytics</span>,
+        <span className="badge badge-pill badge-primary">Google Search Console</span>.</li>
+                <li>
+                  <span className="badge badge-pill badge-primary">Adobe Creative Suite</span> (Illustrator, Photoshop, Fireworks, Premiere
+        Pro, InDesign).</li>
+                <li>Having good analytical, problem-solving skills, ability to meet deadlines and work on multiple projects.</li>
+                <li>A highly motivated team player with an ability to quickly adapt to new environments.</li>
+                <li>Enthusiastic in working with new technologies and new applications.</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="clearfix">&nbsp;</div>
+          <div className="clearfix">&nbsp;</div>
+
+          <div className="row">
+            <div className="col">
+
+              <h3>Professional Work Experience</h3>
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">Company</th>
+                    <th scope="col">Tenure</th>
+                    <th scope="col">Worked</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Workplace Options, Bangalore</td>
+                    <td>April 2018 - Till Date</td>
+                    <td id="totExp"></td>
+                  </tr>
+                  <tr>
+                    <td>Sobha Ltd, Bangalore</td>
+                    <td>July 2015 - April 2018</td>
+                    <td>2.9 Years</td>
+                  </tr>
+                  <tr>
+                    <td>Zyoin, Bangalore</td>
+                    <td>July 2013 - July 2015</td>
+                    <td>2 Years</td>
+                  </tr>
+                  <tr>
+                    <td>Infoinsight Software Solutions, Bangalore</td>
+                    <td>Nov 2011 - July 2013</td>
+                    <td>1.8 Years</td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+            </div>
+          </div>
+
+          <div className="clearfix">&nbsp;</div>
+          <div className="clearfix">&nbsp;</div>
+
+          <div className="row">
+            <div className="col">
+              <h3>Project Highlights</h3>
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">Project Link</th>
+                    <th scope="col">Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.apt212.com" target="blank">www.apt212.com</a>
+                    </td>
+                    <td>Created the front-end using HTML, CSS, AngularJS (v1.6), jQuery and Bootstrap.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.cookbooth.com" target="blank">www.cookbooth.com</a>
+                    </td>
+                    <td>Created the UI using AngularJS (v1.2), HTML5, CSS3, jQuery and Bootstrap.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.phatbuddhawear.com" target="blank">www.phatbuddhawear.com</a>
+                    </td>
+                    <td>Created the front-end of this E-commerce based website using HTML5, CSS3, Bootstrap and jQuery.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.linesheetmaker.com" target="blank">www.linesheetmaker.com</a>
+                    </td>
+                    <td>Worked on front-end as well as backend using PHP, MySQL, HTML, CSS, javascript and jQuery.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.zyoin.com">www.zyoin.com</a>
+                    </td>
+                    <td>Build this site end to end using WordPress with partial usage of bootstrap.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.davidlernerny.com">www.davidlernerny.com</a>
+                    </td>
+                    <td>Built to be an E-commerce website using HTML5, CSS3, javascript, jQuery, PHP, MySql, Shopify and Bootstrap. My
+            role was to create the frontend.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.speechzonenyc.com">www.speechzonenyc.com</a>
+                    </td>
+                    <td>Built to be a core PHP CMS driven website built with HTML, CSS, jQuery, PHP and MySql. My role was to create the
+            frontend using HTML, CSS and jQuery.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.bungeetags.com">www.bungeetags.com</a>
+                    </td>
+                    <td>Built to be an E-commerce website using HTML, CSS, jQuery, .NET and CS-Cart. My role was to create the frontend.</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.soundbytemgmt.com">www.soundbytemgmt.com</a>
+                    </td>
+                    <td>Created this online music portal using with WordPress and bootstrap.</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div className="clearfix">&nbsp;</div>
+          <div className="clearfix">&nbsp;</div>
+
+          <div className="row">
+            <div className="col">
+              <h3>Qualification</h3>
+
+              <table className="table table-bordered">
+                <tbody>
+                  <tr>
+                    <td>B.TECH</td>
+                    <td>2010</td>
+                    <td>72%</td>
+                  </tr>
+                  <tr>
+                    <td>HSC</td>
+                    <td>2006</td>
+                    <td>50%</td>
+                  </tr>
+                  <tr>
+                    <td>SSC</td>
+                    <td>2004</td>
+                    <td>69%</td>
+                  </tr>
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div className="clearfix">&nbsp;</div>
+          <div className="clearfix">&nbsp;</div>
+
+          <div className="row">
+            <div className="col">
+              <h3>Declaration</h3>
+
+              <p>I hereby declare that the above mentioned information is correct up to my knowledge and I bear the responsibility
+      for the correctness of above mentioned particulars.</p>
+            </div>
+          </div>
+
+          <div className="clearfix">&nbsp;</div>
+          <div className="clearfix">&nbsp;</div>
+
+        </div>
+
       </div>
 
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
+      // <div classNameName="App">
+      //   <header classNameName="App-header">
+      //     <img src={logo} classNameName="App-logo" alt="logo" />
+      //     <h1 classNameName="App-title">Welcome to React</h1>
       //   </header>
-      //   <p className="App-intro">
+      //   <p classNameName="App-intro">
       //     To get started, edit <code>src/App.js</code> and save to reload.
       //   </p>
       // </div>
